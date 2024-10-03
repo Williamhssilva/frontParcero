84fd9f9120f9827280f9d83d3d4d0dde9044e502
williamhssilva <williamhss90@gmail.com>
2024-10-03 12:40:55 -0300

    url da api dinamica implementado

--- js/config.js
+++ js/config.js
@@ -1,1 +1,13 @@
-export const API_BASE_URL = 'http://localhost:5000'; // Ajuste conforme necessário
+function getApiBaseUrl() {
+    const hostname = window.location.hostname;
+    if (hostname === 'localhost' || hostname === '127.0.0.1') {
+        return 'http://localhost:5000';
+    } else if (hostname.includes('github.io')) {
+        return 'https://storm-fourth-suede.glitch.me'; // Substitua pela URL real do seu backend no Glitch
+    } else {
+        // Para outros ambientes (como o Glitch), use a URL do backend no Glitch
+        return 'https://storm-fourth-suede.glitch.me'; // Substitua pela URL real do seu backend no Glitch
+    }
+}
+
+export const API_BASE_URL = getApiBaseUrl();
